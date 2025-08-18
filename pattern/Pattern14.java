e7c0a0aee4488e126d561ef71f1ae19c4e9b1f2c
srija-80 <srijacs86@gmail.com>
2025-08-18 19:15:36 +0530

    Pattern14.java

---
+++ pattern/Pattern14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Pattern14{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int count=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(count+" ");
+            }
+            count++;
+            System.out.println();
+        }
+    }
+}
